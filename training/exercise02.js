ec654cdce45076eae20dfba62a84eea284fb8f58
Stum <levistum@byui.edu>
2018-10-15 14:55:27 -0600

    Updated it to work on exercise 2

--- training/exercise02.js
+++ training/exercise02.js
@@ -6,9 +6,10 @@ let sum = 0;
 for (i; i < process.argv.length; i++) {
     sum += +process.argv[i];
 }
+console.log(sum);
 
 // Using Array Methods
-let sum = process.argv
-    .filter((e, i) => i > 1)
-    .reduce((sum, entry) => sum += +entry, 0);
-console.log(sum);
+// let sum = process.argv
+//     .filter((e, i) => i > 1)
+//     .reduce((sum, entry) => sum += +entry, 0);
+// console.log(sum);
